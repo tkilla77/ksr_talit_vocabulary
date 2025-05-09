f90831531ff7dce2c2aa64f736785dbc2a177f0f
Tom Hofmann <tom@scheidweg.net>
2025-05-09 09:57:07 +0200

    better formatting

--- src/static/learn.js
+++ src/static/learn.js
@@ -7,29 +7,34 @@ async function handleSubmit(event) {
     let word1 = word1span.textContent
     let word2 = word2input.value
 
-    let resp = await fetch(`submit/${word1}/${word2}`)
-    if (resp.ok) {
-        let json = await resp.json()
-        form.querySelector(".verdict").textContent = json.correct ? "Correct" : "Incorrect"
-        if (json.correct) {
-            form.querySelector(".correction").innerHTML = "&nbsp;"
+    if (word2) {
+        let resp = await fetch(`submit/${word1}/${word2}`)
+        if (resp.ok) {
+            let json = await resp.json()
+            form.querySelector(".verdict").textContent = json.correct ? "Correct" : "Incorrect"
+            if (json.correct) {
+                form.querySelector(".correction").innerHTML = "&nbsp;"
+            } else {
+                form.querySelector(".correction").textContent = `The correct translation for '${word1} 'is '${json.pair.word2}'`
+            }
+            nextWord(form)
         } else {
-            form.querySelector(".correction").textContent = `The correct translation for '${word1} 'is '${json.pair.word2}'`
+            console.log(resp)
         }
     }
-
-    nextWord(form)
 }
 
 async function nextWord(form) {
     let word1span = form.querySelector(".word1")
     let word2input = form.querySelector(".word2")
 
-    resp = await fetch(`word`)
+    let resp = await fetch(`word`)
     if (resp.ok) {
         let json = await resp.json()
         word1span.textContent = json
         word2input.value = ""
+    } else {
+        console.log(resp)
     }
 }
 
